80ca302fbe49c8527ef9dfef7c8c58dd37849e83
Steve Warren <steve@interactivenyc.com>
2018-11-14 14:30:13 -0500

    incremental change to board.js

--- client/board.js
+++ client/board.js
@@ -8,39 +8,39 @@ class Board {
 
   makeResources(){
     
-    this.resources[11] = new Resource(11, "forest", this)
-    this.resources[12] = new Resource(12, "field", this)
-    this.resources[13] = new Resource(13, "mountain", this)
+    this.resources[11] = new Resource(1, 1, "forest", this)
+    this.resources[12] = new Resource(1, 2, "field", this)
+    this.resources[13] = new Resource(1, 3, "mountain", this)
 
-    this.resources[21] = new Resource(21, "hill", this)
-    this.resources[22] = new Resource(22, "pasture", this)
-    this.resources[23] = new Resource(23, "hill", this)
-    this.resources[24] = new Resource(24, "mountain", this)
+    this.resources[21] = new Resource(2, 1, "hill", this)
+    this.resources[22] = new Resource(2, 2, "pasture", this)
+    this.resources[23] = new Resource(2, 3, "hill", this)
+    this.resources[24] = new Resource(2, 4, "mountain", this)
 
-    this.resources[31] = new Resource(31, "mountain", this)
-    this.resources[32] = new Resource(32, "pasture", this)
-    this.resources[33] = new Resource(33, "center", this)
-    this.resources[34] = new Resource(34, "field", this)
-    this.resources[35] = new Resource(35, "pasture", this)
+    this.resources[31] = new Resource(3, 1, "mountain", this)
+    this.resources[32] = new Resource(3, 2, "pasture", this)
+    this.resources[33] = new Resource(3, 3, "center", this)
+    this.resources[34] = new Resource(3, 4, "field", this)
+    this.resources[35] = new Resource(3, 5, "pasture", this)
 
-    this.resources[41] = new Resource(41, "hill", this)
-    this.resources[42] = new Resource(42, "forest", this)
-    this.resources[43] = new Resource(43, "field", this)
-    this.resources[44] = new Resource(44, "forest", this)
+    this.resources[41] = new Resource(4, 1, "hill", this)
+    this.resources[42] = new Resource(4, 2, "forest", this)
+    this.resources[43] = new Resource(4, 3, "field", this)
+    this.resources[44] = new Resource(4, 4, "forest", this)
 
-    this.resources[51] = new Resource(51, "field", this)
-    this.resources[52] = new Resource(52, "forest", this)
-    this.resources[53] = new Resource(53, "pasture", this)
+    this.resources[51] = new Resource(5, 1, "field", this)
+    this.resources[52] = new Resource(5, 2, "forest", this)
+    this.resources[53] = new Resource(5, 3, "pasture", this)
   }
 }
 
 
 
 class Resource {
-  constructor(id, type, board) {
-    
-
-    this.id = id
+  constructor(row, column, type, board) {
+    this.id = row.toString() + column.toString()
+    this.row = row
+    this.column = column
     this.type = type
     this.diceTarget = null
     this.vertices = []
@@ -50,15 +50,21 @@ class Resource {
   }
 
   setVertices(board) {
-    const firstIndex = Math.floor(this.id/10)
-    const secondIndex = this.id % 10
-    console.log('1 - 2 : ', this.id)
-    this.vertices.push(new Vertex(firstIndex, secondIndex*2-1, board))
-    this.vertices.push(new Vertex(firstIndex, secondIndex*2, board))
-    this.vertices.push(new Vertex(firstIndex, secondIndex*2+1, board))
-    this.vertices.push(new Vertex((firstIndex+1), secondIndex*2-1, board))
-    this.vertices.push(new Vertex((firstIndex+1), secondIndex*2, board))
-    this.vertices.push(new Vertex((firstIndex+1), secondIndex*2+1, board))
+    this.getOrCreateVertex(this.row, this.column*2-1, board)
+    this.getOrCreateVertex(this.row, this.column*2, board)
+    this.getOrCreateVertex(this.row, this.column*2+1, board)
+    this.getOrCreateVertex((this.row+1), this.column*2-1, board)
+    this.getOrCreateVertex((this.row+1), this.column*2, board)
+    this.getOrCreateVertex((this.row+1), this.column*2+1, board)
+  }
+
+  getOrCreateVertex(row, column, board) {
+    const existingVertex = board.vertices[row.toString()+column.toString()]
+    if (existingVertex){
+      this.vertices.push(existingVertex)
+    } else {
+      this.vertices.push(new Vertex(row, column, board))
+    }
   }
 
   setEdges() {
@@ -70,14 +76,13 @@ class Resource {
 
 
 class Vertex {
-  constructor(row, col, board) {
-    const stringId = row.toString() + col.toString()
-    console.log('vertex id:', stringId);
-    
-    this.id = stringId
+  constructor(row, column, board) {
+    this.id = row.toString() + column.toString()
+    this.row = row
+    this.column = column
     this.player = null
     this.locationType = null
-    board.vertices[stringId] = this
+    board.vertices[this.id] = this
   }
 }
 
