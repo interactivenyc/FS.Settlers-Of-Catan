49528bf1f8c91032266294bb5e99340a38b9d3cd
Ryan <rynpowers@gmail.com>
2018-11-14 11:32:24 -0500

    added base files for map components

---
+++ client/components/GameMap/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class GameMap extends Component {
+  render() {
+    return (
+      <div>
+        This is the map
+      </div>
+    )
+  }
+}
+
+export default GameMap;
