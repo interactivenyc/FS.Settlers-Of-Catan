592a6b1fd0d2639ad867091c0e6dbe2a4a4887d6
Steve Warren <steve@interactivenyc.com>
2018-11-14 16:07:15 -0500

    fixing beginner map setup with diceTarget

--- client/board.js
+++ client/board.js
@@ -8,29 +8,29 @@ class Board {
 
   makeResources(){
     
-    this.resources[11] = new Resource(1, 1, "forest", this)
-    this.resources[12] = new Resource(1, 2, "field", this)
-    this.resources[13] = new Resource(1, 3, "mountain", this)
+    this.resources[11] = new Resource(1, 1, 11, "forest", this)
+    this.resources[12] = new Resource(1, 2, 12, "pasture", this)
+    this.resources[13] = new Resource(1, 3, 9, "field", this)
 
-    this.resources[21] = new Resource(2, 1, "hill", this)
-    this.resources[22] = new Resource(2, 2, "pasture", this)
-    this.resources[23] = new Resource(2, 3, "hill", this)
-    this.resources[24] = new Resource(2, 4, "mountain", this)
+    this.resources[21] = new Resource(2, 1, 4, "hill", this)
+    this.resources[22] = new Resource(2, 2, 6, "mountain", this)
+    this.resources[23] = new Resource(2, 3, 5, "hill", this)
+    this.resources[24] = new Resource(2, 4, 10, "pasture", this)
 
-    this.resources[31] = new Resource(3, 1, "mountain", this)
-    this.resources[32] = new Resource(3, 2, "pasture", this)
-    this.resources[33] = new Resource(3, 3, "center", this)
-    this.resources[34] = new Resource(3, 4, "field", this)
-    this.resources[35] = new Resource(3, 5, "pasture", this)
+    this.resources[31] = new Resource(3, 1, 0, "desert", this)
+    this.resources[32] = new Resource(3, 2, 3, "forest", this)
+    this.resources[33] = new Resource(3, 3, 11, "field", this)
+    this.resources[34] = new Resource(3, 4, 4, "forest", this)
+    this.resources[35] = new Resource(3, 5, 8, "field", this)
 
-    this.resources[41] = new Resource(4, 1, "hill", this)
-    this.resources[42] = new Resource(4, 2, "forest", this)
-    this.resources[43] = new Resource(4, 3, "field", this)
-    this.resources[44] = new Resource(4, 4, "forest", this)
+    this.resources[41] = new Resource(4, 1, 8, "hill", this)
+    this.resources[42] = new Resource(4, 2, 10, "pasture", this)
+    this.resources[43] = new Resource(4, 3, 9, "pasture", this)
+    this.resources[44] = new Resource(4, 4, 3, "mountain", this)
 
-    this.resources[51] = new Resource(5, 1, "field", this)
-    this.resources[52] = new Resource(5, 2, "forest", this)
-    this.resources[53] = new Resource(5, 3, "pasture", this)
+    this.resources[51] = new Resource(5, 1, 5, "mountain", this)
+    this.resources[52] = new Resource(5, 2, 2, "field", this)
+    this.resources[53] = new Resource(5, 3, 6, "forest", this)
   }
 
   getStringFromCoordinate(row, column) {
@@ -41,12 +41,12 @@ class Board {
 
 
 class Resource {
-  constructor(row, column, type, board) {
+  constructor(row, column, diceTarget, type, board) {
     this.id = board.getStringFromCoordinate(row, column)
     this.row = row
     this.column = column
     this.type = type
-    this.diceTarget = null
+    this.diceTarget = diceTarget
     this.vertices = []
     this.setVertices(board)
     this.edges = []
