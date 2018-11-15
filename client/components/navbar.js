2136985230af9df4f4b532c888e3aae7c5719d40
Patrick Monaghan <monaghanp3@gmail.com>
2018-11-15 12:59:04 -0500

    navbar changes

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,32 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { logout } from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = ({ handleClick, isLoggedIn }) => (
+  // <div>
+  //   <h1>BOILERMAKER</h1>
+  //   <nav>
+  //     {isLoggedIn ? (
+  //       <div>
+  //         {/* The navbar will show these links after you log in */}
+  //         <Link to="/home">Home</Link>
+  //         <a href="#" onClick={handleClick}>
+  //           Logout
+  //         </a>
+  //       </div>
+  //     ) : (
+  //       <div>
+  //         {/* The navbar will show these links before you log in */}
+  //         <Link to="/login">Login</Link>
+  //         <Link to="/signup">Sign Up</Link>
+  //       </div>
+  //     )}
+  //   </nav>
+  //   <hr />
+  // </div>
   <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
+    <div class="ui text menu">
+      <div class="item">
+        <img src="/images/new-school.jpg" />
+      </div>
+      <a class="browse item">
+        Menu
+        <i class="dropdown icon"></i>
+      </a>
+    </div>
   </div>
 )
+
 
 /**
  * CONTAINER
@@ -48,8 +60,8 @@ const mapDispatch = dispatch => {
 export default connect(mapState, mapDispatch)(Navbar)
 
 /**
- * PROP TYPES
- */
+* PROP TYPES
+*/
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
