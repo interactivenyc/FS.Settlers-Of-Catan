07e76203cae5179936ab8e581ed5010067ac0786
Steve Warren <steve@interactivenyc.com>
2018-11-16 11:23:49 -0500

    moving GameLobby css to local file

--- client/components/GameLobby.js
+++ client/components/GameLobby.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import socket from '../socket'
+import './GameLobby.css'
 
 export class GameLobby extends React.Component {
   constructor(props) {
