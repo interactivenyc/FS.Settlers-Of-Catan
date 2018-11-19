3a378d212c5b1b8bedf04a9fef6337117067983e
Patrick Monaghan <monaghanp3@gmail.com>
2018-11-19 15:45:55 -0500

    added exit command to test script

--- client/socket.js
+++ client/socket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 
 if (process.env.NODE_ENV === 'test') {
-  global.window = {location: {origin: ''}}
+  global.window = {location: {origin: 'http://localhost:8080'}}
 }
 
 const socket = io(window.location.origin)
