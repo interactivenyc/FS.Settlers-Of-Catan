4586cf08552a2fb16d6a29e750503c523d4a04dd
Patrick Monaghan <monaghanp3@gmail.com>
2018-11-19 15:29:21 -0500

    fixed tests

--- client/socket.js
+++ client/socket.js
@@ -1,5 +1,9 @@
 import io from 'socket.io-client'
 
+if (process.env.NODE_ENV === 'test') {
+  global.window = {location: {origin: ''}}
+}
+
 const socket = io(window.location.origin)
 
 socket.on('connect', () => {
