39d9ae12149f0aabebdf2b57a779f0dcc6ba96a3
Patrick Monaghan <monaghanp3@gmail.com>
2018-11-21 17:47:37 -0500

    modal portal has switch statement

--- client/components/GameMap/Hexagon/Edges.js
+++ client/components/GameMap/Hexagon/Edges.js
@@ -17,76 +17,85 @@ class Edges extends React.Component {
     }
   }
 
-  handleMouseEnter = event => {
-    //need to be able to access hidden divs
-    console.log(event.target)
-    const {edges, player, board} = this.props
-    const edge = edges[event.target.id]
-    const neighbors = getEdgeNeighborsColor(edge, board)
+  componentDidMount() {
+    const {sides, edges, board, player} = this.props
+    for (const side in sides) {
+      const edge = edges[sides[side]]
+      const neighbors = getEdgeNeighborsColor(edge, board)
 
-    if (validateChangeEdge(player, edge, neighbors, board)) {
-      const targetEdgeId = event.target.getAttribute('stateid')
-      this.setState({[targetEdgeId]: player.color})
+      if (validateChangeEdge(player, edge, neighbors, board)) {
+        this.setState({[side]: `build-player-${player.playerNumber}`})
+      }
     }
   }
 
-  handleMouseLeave = event => {
-    const targetEdgeId = event.target.getAttribute('stateid')
-    this.setState({[targetEdgeId]: null})
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      const {sides, edges, board, player} = this.props
+      for (const side in sides) {
+        const edge = edges[sides[side]]
+        const neighbors = getEdgeNeighborsColor(edge, board)
+
+        if (validateChangeEdge(player, edge, neighbors, board)) {
+          this.setState({[side]: `build-player-${player.playerNumber}`})
+        }
+      }
+    }
   }
+
+  // handleMouseEnter = event => {
+  //   //need to be able to access hidden divs
+  //   const { edges, player, board } = this.props
+  //   const edge = edges[event.target.id]
+  //   const neighbors = getEdgeNeighborsColor(edge, board)
+
+  //   if (validateChangeEdge(player, edge, neighbors, board)) {
+  //     const targetEdgeId = event.target.getAttribute('stateid')
+  //     this.setState({ [targetEdgeId]: `build-player-${player.playerNumber}` })
+  //   }
+  // }
+
+  // handleMouseLeave = event => {
+  //   const targetEdgeId = event.target.getAttribute('stateid')
+  //   this.setState({ [targetEdgeId]: null })
+  // }
 
   render() {
     const {sides, edges} = this.props
 
     return (
       <Fragment>
-        <div
-          className="row"
-          onMouseEnter={event => this.handleMouseEnter(event)}
-          onMouseLeave={event => this.handleMouseLeave(event)}
-        >
+        <div className="row">
           <div
             id={sides[1]}
-            className={`side side-1 ${edges[sides[1]].color}`}
-            stateid={1}
-            style={{backgroundColor: this.state[1]}}
+            className={`side side-1 ${edges[sides[1]].color} ${this.state[1]}`}
           />
           <div
             id={sides[2]}
-            className={`side side-2 ${edges[sides[2]].color}`}
-            stateid={2}
-            style={{backgroundColor: this.state[2]}}
+            className={`side side-2 ${edges[sides[2]].color} ${this.state[2]}`}
           />
         </div>
         <div className="row row-middle">
           <div
             id={sides[3]}
-            className={`side side-3 ${edges[sides[3]].color}`}
-            stateid={3}
-            style={{backgroundColor: this.state[3]}}
+            className={`side side-3 ${edges[sides[3]].color} ${this.state[3]}`}
           />
           <div
             id={sides[4]}
             className={`side side-4 ${edges[sides[4]] &&
-              edges[sides[4]].color}`}
-            stateid={4}
-            style={{backgroundColor: this.state[4]}}
+              edges[sides[4]].color} ${this.state[4]}`}
           />
         </div>
         <div className="row">
           <div
             id={sides[5]}
             className={`side side-5 ${edges[sides[5]] &&
-              edges[sides[5]].color}`}
-            stateid={5}
-            style={{backgroundColor: this.state[5]}}
+              edges[sides[5]].color} ${this.state[5]}`}
           />
           <div
             id={sides[6]}
             className={`side side-6 ${edges[sides[6]] &&
-              edges[sides[6]].color}`}
-            stateid={6}
-            style={{backgroundColor: this.state[6]}}
+              edges[sides[6]].color} ${this.state[6]}`}
           />
         </div>
       </Fragment>
