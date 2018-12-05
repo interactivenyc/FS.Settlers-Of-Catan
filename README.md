2937178003abc4c009db7e2560deea10e9c399a0
Frank <frankamalafronte@gmail.com>
2018-12-05 16:25:13 -0500

    readmen and final draft of demo modal

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+###Settlers of Fullstack
+
 Thank you for visiting Settlers of Fullstack, a digital port of the popular strategy board game Settlers of Catan.
 
 Settlers of Fullstack was built by Frank Malafronte, Ryan Powers, Patrick Monaghan, and Steven Warren as a capstone project at Fullstack Academy.
@@ -6,4 +8,7 @@ It was built over 10 coding days in the fall of 2018.
 You can visit our deployed app at settlers-of-fullstack.herokuapp.com
 Once on the page, you can view a single player Demo version of the game by navigating to the "How To" Link.
 This Demo will allow you to test drive some of the features of the game without needing to start a four player game.
-A full game requires four logged in users to join a game in the game lobby. If you would like to experience all our game has to offer
+A full game requires four logged in users to join a game in the game lobby. If you would like to experience all our game has to offer,
+bring along some friends!
+
+You can also see an overview of our project here: https://www.youtube.com/watch?v=P1EjlFxdpCk&index=8&list=PLx0iOsdUOUmmMdgcWgABSEBRkSWAG3aNP&t=0s&fbclid=IwAR3Xe4ZiLN0mYK1Rte732_pCnZrMlwohCPF7QMvesPWYE0vOwvFLOG0z24c
--- client/components/GameMap/Modle/Demo.js
+++ client/components/GameMap/Modle/Demo.js
@@ -8,11 +8,20 @@ export default class Demo extends Component {
   render() {
     return (
       <div>
-        <div>Welcome to the Settlers of FullStack Demo Page!</div>
+        <button
+          type="button"
+          onClick={() => this.props.toggleModal(false)}
+          style={{float: 'right', fontSize: '10pt'}}
+        >
+          X
+        </button>
+        <div style={{margin: '5px'}}>
+          Welcome to the Settlers of FullStack Demo Page!
+        </div>
         <div>
           <h1 />
         </div>
-        <div>
+        <div style={{margin: '5px'}}>
           In this Demo you can test out some of the features of our game, such
           as rolling the dice to gain resources and building settlements,
           without starting a full four-player game.
@@ -20,14 +29,22 @@ export default class Demo extends Component {
         <div>
           <h1 />
         </div>
-        <div>
+        <div style={{margin: '5px'}}>
           In Demo Mode move verification is turned off, so roads settlements and
           cities can be placed anywhere,
         </div>
 
-        <div>
+        <div style={{margin: '5px'}}>
           but we still want to show off some game logic, so all moves still
           require the correct resources!
+        </div>
+        <div>
+          <h1 />
+        </div>
+        <div style={{margin: '5px'}}>
+          A full game requires four logged in users to join a game in the game
+          lobby. If you would like to experience all our game has to offer,
+          bring along some friends!
         </div>
       </div>
     )
